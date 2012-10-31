004592aa5d6055db9f5868eb486703d116125582
Filip Tepper <ft@taptera.com>
2012-10-31 23:23:52 +0100

    added sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,11 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   protected
 
   def auth_hash
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Lunchzilla::Application.routes.draw do
+  resource :sessions, only: [:destroy]
+
   get '/auth/:provider/callback', to: 'sessions#create'
   root to: 'home#show'
 end
